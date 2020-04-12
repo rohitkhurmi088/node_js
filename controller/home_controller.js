8d36b2c70a4703c6e6abe8d55f11e6d1e01e2e45
rk <rohit.khurmi088@gmail.com>
2020-04-12 19:31:43 +0530

    Display Post : with related user (mongoose.populate())

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,18 +1,43 @@
 //_________home_controller_________
 
-module.exports.home = ('/', (req,res)=>{
+//importing Post Model
+const Post = require('../models/post');
 
+
+module.exports.home = (req,res)=>{
 
     /***______{cookies}____****
     //priniting cookies in console
     console.log(req.cookies);
     //Altering cookie value
     res.cookie('user_rk', 27); */
+    
+    //____________________________________________ 
+    //____Displaying Posts by user on homepage_____
+   //______________________________________________
+   /* Model.Find({}, func(err, parameter){}) */
+    //Post.find({}, function(err,posts){
+        //return res.render('home',{
+            title:'Homepage',
+           //posts:posts //displaying posts
+        //});
+    //});
 
-    return res.render('home',{
-        title:'Homepage'
-    });
-});
+    //__________With POPULATING(user object)__________________
+    /*Displaying Post created user by name,email--> in home.ejs
+      Pre-Populating reffered user object  using mongoose.populate
+      Populating: used to get user object :name,email,password
+      from another schema (User) in the current Schema (Post)
+    Model.find({}).populate('object to get('object-from-anotherSchema').exec(callback func)*/
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title:'HomePage',
+            posts: posts //displaying posts
+        })
+    }); 
+   
+}
 
 //For Exporting Function
 //module.exports.actionName = func(req,res){}
